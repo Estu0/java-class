cdefe0aec33b17c4b1238eda997876aed1329829
Estuardo Joel Higueros Reyes <estuar0jhr@gmail.com>
2022-08-07 21:09:36 -0400

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *@author Estuardo Joel Higueros Reyes
+ *email: estuar0jhr@gmail.com
+ *date: 8/7/022
+ *purpose: My first Hello World program in an IDE
+ */
+
+//ejhr
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Estuardo!");
+    }
+}
